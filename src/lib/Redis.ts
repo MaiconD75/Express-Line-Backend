be8babe5fc6aeb66d997da1f7ef076f05f59b491
maicond75 <maicondouglas75@hotmail.com>
2021-08-31 17:42:41 -0300

    feat/add-deliverymen-to-cache

--- src/lib/Redis.ts
+++ src/lib/Redis.ts
@@ -8,9 +8,21 @@ export default class RedisCache {
     this.redis = new Redis(cacheConfig.config.redis);
   }
 
-  // public async save(key: string, value: string): Promise<void> {}
+  public async save(key: string, value: unknown): Promise<void> {
+    await this.redis.set(key, JSON.stringify(value));
+  }
 
-  // public async recover(key: string): Promise<void> {}
+  public async recover<T>(key: string): Promise<T | null> {
+    const data = await this.redis.get(key);
+
+    if (!data) {
+      return null;
+    }
+
+    const parsedData = JSON.parse(data) as T;
+
+    return parsedData;
+  }
 
   // public async invalidate(key: string): Promise<void> {}
 }
