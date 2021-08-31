ab6552dd250a34b13aea978b7ffc0fa2939275d5
maicond75 <maicondouglas75@hotmail.com>
2021-08-31 11:13:16 -0300

    feat/add-cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import './database';
@@ -12,6 +13,8 @@ import uploadConfig from './config/upload';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
